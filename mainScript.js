fbf3ec12774eecb4bed2097d7252e6908d2392b7
Calandda <A.calandda@gmail.com>
2025-03-08 14:29:31 +0800

    Test commit 2

--- mainScript.js
+++ mainScript.js
@@ -1,6 +1,6 @@
 import dom from "./DOM/dom.js";
 import internal from "./Internal/internal.js";
-let test = 'V6WSYN9KCB6WN4MBMB2ADE4ES';
+let test = '57DWYW9UQ3UJFFH63FH37X5SH';
 dom.domTest();
 internal.internalTest();
 console.log('test');
