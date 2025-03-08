6d99f487aa8c7fc6b795195edc1d125b097da1b0
Calandda <A.calandda@gmail.com>
2025-03-08 17:25:25 +0800

    fixed Weather API call

--- internal/internal.js
+++ internal/internal.js
@@ -8,7 +8,7 @@ class internalObject{
         let KEY = 'PSEGYPSPDNJ2EUM3AMTFCUEWC';
         let response;
         if(startDate == null && endDate == null){
-            response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${KEY}`);
+            response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${KEY}`);
         } else {
             response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${startDate}/${endDate}?key=${KEY}`);
         }
--- mainScript.js
+++ mainScript.js
@@ -1,7 +1,6 @@
 import dom from "./DOM/dom.js";
 import internal from "./Internal/internal.js";
 let date = new Date();
-dom.domTest();
-internal.internalTest('philippines',null,null);
-internal.internalTest('philippines',date.getTime()/1000,date.getTime()/1000);
-internal.getWeather();
+
+internal.getWeather('Philippines',null,null);
+internal.getWeather('philippines',parseInt(date.getTime()/1000),parseInt(date.getTime()/1000));
